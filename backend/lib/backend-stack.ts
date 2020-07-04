e9b93cc8250798a7e19dbfb8da054e070d6e5482
Hiroya Akita <akky.develop@gmail.com>
2020-07-05 01:26:37 +0900

    fix: update backend code

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,9 +1,40 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import { RemovalPolicy } from "@aws-cdk/core";
+import { Table, AttributeType } from "@aws-cdk/aws-dynamodb";
+import { Function, Runtime, Code } from "@aws-cdk/aws-lambda";
+import { RestApi, LambdaIntegration, Cors } from "@aws-cdk/aws-apigateway";
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const suffix = "sample";
+    const fn = new Function(this, "Function", {
+      functionName: `selfIntroductionTutorial_${suffix}`,
+      runtime: Runtime.PYTHON_3_8,
+      handler: "index.handler",
+      code: Code.fromAsset("lambda"),
+      environment: {
+        TABLE_NAME: `self_introduction_tutorial_${suffix}`,
+      },
+    });
+
+    const table = new Table(this, "Table", {
+      tableName: `self_introduction_tutorial_${suffix}`,
+      partitionKey: { name: "id", type: AttributeType.STRING },
+      readCapacity: 1,
+      writeCapacity: 1,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+    // Lambda関数にテーブルの読み書き権限を付与
+    table.grantReadWriteData(fn);
+
+    const api = new RestApi(this, "Api", {
+      restApiName: `Self Introduction Tutorial ${suffix}`,
+      description: "自己紹介チュートリアル用API",
+    });
+    const integration = new LambdaIntegration(fn);
+    const userData = api.root.addResource("user-data");
+    userData.addMethod("POST", integration);
   }
 }
